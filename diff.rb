c0be005e82cf92287a026492000f59def214af5f
Be <bebirchall@gmail.com>
2013-08-10 18:16:42 -0400

    command line interaction, refactor into classes

--- diff.rb
+++ diff.rb
@@ -1,87 +1,115 @@
-require_relative 'colour'
-require_relative 'lcs'
 
-# lcs(a,b) ==> [['x',1,2], ['y',3,3]]
+class Versions
+  attr_accessor :original, :revised
 
+  def initialize(original, revised)
+    @original = original
+    @revised = revised
+  end
 
-
-
-
-
-
-def diff(original, modified) #array --> string
-  array = []
-  hash = match(original, modified)
-  (0...length_diff(original, modified)).each do |i|
-    if hash.has_key?(i)
-      array << original[i]
+  def lcs(i=0, j=0, memo={})
+    return memo[[original, revised, i, j]] if memo.has_key?([original, revised, i, j])
+    return [] if original.empty? || revised.empty?
+    if original.first == revised.first
+      [[original.first, i, j]] + 
+          Versions.new(original.drop(1), revised.drop(1)).lcs(i+1, j+1, memo)
     else
-      array << red(original[i])
-    
+      seq1 = memo[[original.drop(1), revised, i+1, j]] = 
+        Versions.new(original.drop(1), revised).lcs(i+1, j, memo)
+      seq2 = memo[[original, revised.drop(1), i, j+1]] =
+        Versions.new(original, revised.drop(1)).lcs(i, j+1, memo)
+      if seq1.count >= seq2.count
+        seq1
+      else
+        seq2
+      end
     end
   end
-  array.join
-end
 
-#match: lcs (ordered by index in original)
-def diffs(a,b, match)
-  if match.empty?
-    a.map { |x| red(x) } + b.map { |x| green(x) }
-  elsif match.first[1] == 0 && match.first[2] == 0
-    remaining_match = match.drop(1).map do |triple|
-      [triple[0], triple[1] - 1, triple[2] - 1]
+  def diff(match)
+    if match.empty?
+      original.map { |x| Display.deleted(x) } + revised.map { |x| Display.added(x) }
+    elsif match.first[1] == 0 && match.first[2] == 0
+      remaining_match = match.drop(1).map { |t| [t[0], t[1]-1, t[2]-1] }
+      [original[0]] + Versions.new(original.drop(1), revised.drop(1)).diff(remaining_match)
+    elsif match.first[1] == 0
+      remaining_match = match.map { |t| [t[0], t[1], t[2]-1] }
+      [Display.added(revised[0])] + Versions.new(original, revised.drop(1)).diff(remaining_match)
+    else match.first[2] == 0
+      remaining_match = match.map { |t| [t[0], t[1]-1, t[2]]}
+      [Display.deleted(original[0])] + Versions.new(original.drop(1), revised).diff(remaining_match)
     end
-    puts remaining_match.inspect
-    [a[0]] + diffs(a.drop(1), b.drop(1), remaining_match)
-  elsif match.first[1] == 0
-    [red(b[0])] + diffs(a, b.drop(1), match.map{ |t| [t[0], t[1], t[2] - 1] })
-  else match.first[2] == 0
-    [a[0]] + diffs(a.drop(1), b, match.map{ |t| [t[0], t[1] - 1, t[2]] })
+  end
+
+end
+
+module Display
+  def self.deleted(x)
+    red(x)
+  end
+
+  def self.added(x)
+    green(x)
+  end
+
+  def self.colour(text, code)
+    "\e[#{code}m#{text}\e[0m"
+  end
+
+  def self.red(text)
+    colour(text, 31)
+  end
+
+  def self.green(text)
+    colour(text, 32)
   end
 end
 
+class GiveDiff
+  attr_accessor :original, :revised
 
-
-
-def match(a,b)
-  lcs(a,b).sort { |x,y| x[1] <=> y[1] }
-end
-
-
-
-
-def next_key(i, hash)
-  key = i + 1
-  until hash.has_key?(key)
-    key += 1
+  def compare
+    get_original
+    get_revised
+    process
+    output_diff
   end
-  key
-end
 
-def length_diff(original, modified)
-  original.length + modified.length - lcs(original, modified).length
-end
-
-
-def match(original, modified)
-  lcs = lcs(original, modified)
-  hash = {}
-  lcs.each do |triple|
-    hash[triple[1]] = triple[2]
+  def get_original
+    puts 'Enter your first text:'
+    print '> '
+    first = gets.chomp
+    @original = first.split('')
   end
-  hash
+
+  def get_revised
+    puts 'Enter your revised text:'
+    print '> '
+    second = gets.chomp
+    @revised = second.split('')
+  end
+
+  def process
+    v = Versions.new(@original, @revised)
+    v.diff(v.lcs)
+  end
+
+  def output_diff
+    puts process.join
+  end
+
 end
 
-# sort triple by second elemnent
-def sort_by_middle(triples)
-  triples.sort { |x,y| x[1] <=> y[1] }
-end
 
 
+# orig = 'hi, how are you today?'.split('')
+# rev = 'hello, how are you?'.split('')
 
-a = %w(c a t)
-b = %w(c t p)
-puts diffs(a, b, lcs(a,b)).join
+
+# v = Versions.new(orig, rev)
+
+# puts v.diff(v.lcs).join
+
 
 
 
---
+++ get_diff.rb
@@ -0,0 +1,4 @@
+require_relative 'diff.rb'
+
+give_diff = GiveDiff.new
+give_diff.compare
