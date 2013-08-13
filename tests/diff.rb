c352dc1176f84dbe0370e04d9f690a668a2be589
Be <bebirchall@gmail.com>
2013-08-13 13:00:57 -0400

    tests

---
+++ tests/diff.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require_relative '../diff'
+
+
+class TestDiff < MiniTest::Unit::TestCase
+
+  def test_same_inputs
+    original = [1,2]
+    revised = [1,2]
+    v = Versions.new(original, revised)
+    match = [[1,0,0], [2,1,1]]
+    assert_equal(v.diff(match), [1,2])
+  end
+
+  def test_just_additions
+    original = [1,2]
+    revised = [1, 3, 2, 4]
+    v = Versions.new(original, revised)
+    match = [[1,0,0], [2,1,2]]
+    assert_equal(v.diff(match), [1, Display.added(3), 2, Display.added(4)])
+  end
+
+  def test_just_deletions
+    original = [1, 3, 2, 4]
+    revised = [1,2]
+    v = Versions.new(original, revised)
+    match = [[1,0,0], [2,2,1]]
+    assert_equal(v.diff(match), [1, Display.deleted(3), 2, Display.deleted(4)])
+  end
+
+  def test_add_and_delete
+    original = [1, 3, 2]
+    revised = [1, 2, 4]
+    v = Versions.new(original, revised)
+    match = [[1,0,0], [2,2,1]]
+    assert_equal(v.diff(match), [1, Display.deleted(3), 2, Display.added(4)])
+  end
+
+end
+
---
+++ tests/lcs.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require_relative '../diff'
+
+
+class TestLcs < MiniTest::Unit::TestCase
+  def test_a_empty
+    original = []
+    revised = [1,2]
+    v = Versions.new(original, revised)
+    assert_equal(v.lcs, [])
+  end
+
+  def test_b_empty
+    original = [1,2,3]
+    revised = []
+    v = Versions.new(original, revised)
+    assert_equal(v.lcs, [])
+  end
+
+  def test_a_subseq_b
+    original = [1,2]
+    revised = [5,1,3,2,5]
+    v = Versions.new(original, revised)
+    assert_equal(v.lcs, ([[1,0,1], [2,1,3]]))
+  end
+
+  def test_a_overlap_b
+    original = [1,2,5]
+    revised = [5,1,3,2,5]
+    v = Versions.new(original, revised)
+    assert_equal(v.lcs, ([[1,0,1], [2,1,3] ,[5,2,4]]))
+  end
+
+end
