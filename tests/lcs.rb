573f439e94ff3abbb1dc5e0a0140990962b18487
Be <bebirchall@gmail.com>
2013-08-14 22:54:57 -0400

    change lcs to longest_common_subseq

--- tests/lcs.rb
+++ tests/lcs.rb
@@ -2,33 +2,33 @@ require 'minitest/autorun'
 require_relative '../diff'
 
 
-class TestLcs < MiniTest::Unit::TestCase
+class TestLongestCommonSubseq < MiniTest::Unit::TestCase
   def test_a_empty
     original = []
     revised = [1,2]
     v = Versions.new(original, revised)
-    assert_equal(v.lcs, [])
+    assert_equal(v.longest_common_subseq, [])
   end
 
   def test_b_empty
     original = [1,2,3]
     revised = []
     v = Versions.new(original, revised)
-    assert_equal(v.lcs, [])
+    assert_equal(v.longest_common_subseq, [])
   end
 
   def test_a_subseq_b
     original = [1,2]
     revised = [5,1,3,2,5]
     v = Versions.new(original, revised)
-    assert_equal(v.lcs, ([[0,1], [1,3]]))
+    assert_equal(v.longest_common_subseq, ([[0,1], [1,3]]))
   end
 
   def test_a_overlap_b
     original = [1,2,5]
     revised = [5,1,3,2,5]
     v = Versions.new(original, revised)
-    assert_equal(v.lcs, ([[0,1], [1,3] ,[2,4]]))
+    assert_equal(v.longest_common_subseq, ([[0,1], [1,3] ,[2,4]]))
   end
 
 end
