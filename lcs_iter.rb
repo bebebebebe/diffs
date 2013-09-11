9c31b6ef86406a527c391b9a3edc426bd91e3654
Be <bebirchall@gmail.com>
2013-09-10 23:42:37 -0400

    refactor, use less parameters

--- lcs_iter.rb
+++ lcs_iter.rb
@@ -1,44 +1,45 @@
 class Versions
-  attr_accessor :string_x, :string_y
+  attr_accessor :v1, :v2
 
-  def initialize(string_x, string_y)
-    @string_x = string_x
-    @string_y = string_y
+  # v1 and v2 are two versions to be compared.
+  # They can be strings or arrays.
+
+  def initialize(v1, v2)
+    @v1 = v1
+    @v2 = v2
   end
 
-  # use table to construct the longest common subsequence of
-  # prefixes of string_x up to index x and string_y up to index y, respectively
-  def longest_common_subsequence(x=string_x.length - 1, y=string_y.length - 1)
+  # use table below to construct the longest common subsequence of
+  # prefixes of v1 up to index x and v2 up to index y, respectively
+  def longest_common_subsequence(x=v1.length - 1, y=v2.length - 1)
     return '' if table[[x,y]] == 0
-    if string_x[x] == string_y[y]
-      make_lcs(string_x[0...x], string_y[0...y], table, x-1, y-1) + string_x[x]
+    if v1[x] == v2[y]
+      longest_common_subsequence(x-1, y-1) + v1[x]
     elsif table[[x-1, y]] >= table[[x, y-1]]
-      make_lcs(string_x[0...x], string_y[0..y], table, x-1, y)
+      longest_common_subsequence(x-1, y)
     else
-      make_lcs(string_x[0..x], string_y[0...y], table, x, y-1)
+      longest_common_subsequence(x, y-1)
     end
   end
 
-  # return table: lcs[i,j] is the length of the longest common subsequence of
-  # the length x prefix of string_x and the length y prefix of string_y.
+  # Returns a hash with key [x,y] having the value
+  # length of the longest common subsequence of
+  # the prefix of v1 up to index x, and the prefix of v2 up to the index y.
   def table
     table = {}
-    (-1...string_x.length).each { |x| table[[x, -1]] = 0 }
-    (0...string_y.length).each { |y| table[[-1, y]] = 0 }
+    (-1...v1.length).each { |x| table[[x, -1]] = 0 }
+    (0...v2.length).each { |y| table[[-1, y]] = 0 }
 
-    (0...string_x.length).each do |x|
-      (0...string_y.length).each do |y|
-        if string_x[x] == string_y[y]
+    (0...v1.length).each do |x|
+      (0...v2.length).each do |y|
+        if v1[x] == v2[y]
           table[[x,y]] = table[[x-1, y-1]] + 1
         else
           table[[x,y]] = [table[[x-1,y]], table[[x,y-1]]].max
         end
       end
     end
-
     table
   end
 
 end
-
-
