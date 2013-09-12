02d1cfe0e1cbe73ec7605c928ed1fd4186b86bc1
Be <bebirchall@gmail.com>
2013-09-12 16:53:01 -0400

    lcs consists of indices of common elements instead of the elements themselves

--- lcs_iter.rb
+++ lcs_iter.rb
@@ -9,13 +9,13 @@ class Versions
     @v2 = v2
   end
 
-  # use table below to construct the longest common subsequence of
-  # prefixes of v1 up to index x and v2 up to index y, respectively
-  # Returns lcs of same type as v1
+  # Uses table below to construct the longest common subsequence of
+  # prefixes of v1 up to index x and v2 up to index y, respectively.
+  # Returns array with elements [x,y] with v1[x] == v2[y] making up the lcs
   def longest_common_subsequence(x=v1.length - 1, y=v2.length - 1)
-    return v1.class.new if table[[x,y]] == 0
+    return [] if table[[x,y]] == 0
     if v1[x] == v2[y]
-      longest_common_subsequence(x-1, y-1) << v1[x]
+      longest_common_subsequence(x-1, y-1) << [x,y]
     elsif table[[x-1, y]] >= table[[x, y-1]]
       longest_common_subsequence(x-1, y)
     else
@@ -23,7 +23,7 @@ class Versions
     end
   end
 
-  # Returns a hash with key [x,y] having the value:
+  # Returns a hash with each key [x,y] having the value:
   # length of the longest common subsequence of
   # the prefix of v1 up to index x, and the prefix of v2 up to the index y.
   def table
