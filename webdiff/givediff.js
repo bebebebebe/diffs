d98eff16ad78ab2d8f64b7de3fc87897c2c62ebc
Be <bebirchall@gmail.com>
2013-09-17 17:06:25 -0400

    html etc files for js version

---
+++ webdiff/givediff.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+  var button = $('#btn');
+  var output = document.getElementById("result");
+  button.on('click', function(){
+    var original = $('#original').val();
+    var revised = $('#revised').val();
+    w = new WebDiff(original, revised);
+    output.innerHTML="Implied edits in going from your first to your revised text:<br>" + w.compare();
+  })
+
+})
