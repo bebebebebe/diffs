8ecefdbb85ad22d0455d14f85c96028b467c2218
Be <bebirchall@gmail.com>
2013-09-23 23:49:12 -0400

    js iterative version

--- webdiff/diff_table.js
+++ webdiff/diff_table.js
@@ -2,42 +2,57 @@ function Versions(v1, v2){
   this.v1 = v1;
   this.v2 = v2;
 
-
-  // table(x,y) is the length of the longest common subsequence of
+  // lcs_table is an array of arrays. lcs_table[x][y] is the length of 
+  // the longest common subsequence of
   // the prefix of v1 up to index x and the prefix of v2 up to the index y.
   // 
-  this.table = function(x,y) {
-    if (x == -1 || y == -1) {
-      return 0;
-    } else if (v1[x] === v2[y]) {
-      return this.table(x-1, y-1) + 1;
-    } else {
-      return Math.max(this.table(x-1, y), this.table(x, y-1));
+  var lcs_table = [];
+  while (lcs_table.length <= v2.length) {
+  lcs_table.push([0]);
+  }
+  while (lcs_table[0].length <= v1.length){
+  lcs_table[0].push(0);
+  }
+  for (x=1; x<=v2.length; x++){
+    for (y=1; y<=v1.length; y++){
+      if (v1[x-1] === v2[y-1]) {
+        lcs_table[x][y] = lcs_table[x-1][y-1] + 1
+      } else {
+        lcs_table[x][y] = Math.max(lcs_table[x-1][y], lcs_table[x][y-1])
+      }
     }
-  };
+  }
 
-  // Use table above to construct the implied edits in going from
-  // the prefix of v1 up to index x, to the previx of v2 up to index y.
-  // 
+  // Uses lcs_table above to construct the implied edits in going from
+  // the prefix of v1 up to index x, to the prefix of v2 up to index y.
+  //
   this.diff = function(x,y) {
     x = typeof x !== 'undefined' ? x : v1.length - 1;
-    y = typeof y !== 'undefined' ? y : v2.length -1;
-    if (x < 0 || y < 0) {
+    y = typeof y !== 'undefined' ? y : v2.length - 1;
+    if (x < 0 && y < 0) {
       return [];
+    } else if (y < 0) {
+      var array = this.diff(x-1, y);
+      array.push(Display.deleted(v1[x]));
+      return array;
+    } else if (x < 0) {
+      var array = this.diff(x, y-1);
+      array.push(Display.added(v2[y]));
+      return array;
     } else if (v1[x] === v2[y]) {
       var array = this.diff(x-1, y-1);
       array.push(v1[x]);
       return array;
-    } else if (this.table(x-1, y) >= this.table(x, y-1)) {
+    } else if (lcs_table[x][y+1] >= lcs_table[x+1][y]) {
       var array = this.diff(x-1, y);
       array.push(Display.deleted(v1[x]));
       return array;
     } else {
-      var array = this.diff(x, y-1);
+      array = this.diff(x, y-1);
       array.push(Display.added(v2[y]));
       return array;
     }
-  };
+  }
 
 };
 
