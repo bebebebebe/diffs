047d971b3aab8eaf69a89830605feaa39ffa7050
Be <bebirchall@gmail.com>
2013-11-19 17:25:19 -0500

    delete mistaken comments from jshint

--- webdiff/diff_table.js
+++ webdiff/diff_table.js
@@ -1,14 +1,3 @@
-// Hello.
-//
-// This is JSHint, a tool that helps to detect errors and potential
-// problems in your JavaScript code.
-//
-// To start, simply enter some JavaScript anywhere on this page. Your
-// report will appear on the right side.
-//
-// Additionally, you can toggle specific options in the Configure
-// menu.
-
 ;(function(exports) {
 
   function lcsTable(v1, v2) {
